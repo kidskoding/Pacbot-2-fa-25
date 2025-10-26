ba562fd55b88ccb621df41f5dac724ac923cf25c
Anirudh Konidala <kidskodingclub@gmail.com>
2025-10-26 14:26:24 -0500

    added pyo3 bindings for ghost_state

---
+++ server/game_rs/src/pyo3_bindings/ghost_state_py.rs
@@ -0,0 +1,77 @@
+use pyo3::prelude::*;
+use crate::ghost_state::{self, GhostState};
+
+#[pyclass]
+pub struct PyGameGhostState {
+    ghost_state: GhostState
+}
+
+#[pymethods]
+impl PyGameGhostState {
+    #[new]
+    pub fn new() -> Self {
+        Self {
+            ghost_state: GhostState::new()
+        } 
+    }
+
+    pub fn setfright_steps(&mut self, steps: u8) {
+        self.ghost_state.setfright_steps(steps) 
+    }
+
+    // Decrement the fright steps of a ghost
+    pub fn decfright_steps(&mut self) {
+        self.ghost_state.decfright_steps()
+    }
+
+    // Get the fright steps of a ghost
+    pub fn getfright_steps(&self) -> u8 {
+        self.ghost_state.getfright_steps()
+    }
+
+    // Check if a ghost is frightened
+    pub fn isFrightened(&self) -> bool {
+        self.ghost_state.isFrightened()
+    }
+
+    /****************************** Ghost Trap State ******************************/
+
+    // Set the trapped steps of a ghost
+    pub fn settrapped_steps(&mut self, steps: u8) {
+        self.ghost_state.settrapped_steps(steps)
+    }
+
+    // Decrement the trapped steps of a ghost
+    pub fn dectrapped_steps(&mut self) {
+        self.ghost_state.dectrapped_steps()
+    }
+
+    // Check if a ghost is trapped
+    pub fn isTrapped(&self) -> bool {
+        self.ghost_state.isTrapped()
+    }
+
+    /**************************** Ghost Spawning State ****************************/
+
+    // Set the ghost spawning flag
+    pub fn setSpawning(&mut self, spawning: bool) {
+        self.ghost_state.setSpawning(spawning)
+    }
+
+    // Check if a ghost is spawning
+    pub fn isSpawning(&self) -> bool {
+        self.ghost_state.isSpawning()
+    }
+
+    /****************************** Ghost Eaten Flag ******************************/
+
+    // Set the ghost eaten flag
+    pub fn setEaten(&mut self, eaten: bool) {
+        self.ghost_state.setEaten(eaten)
+    }
+
+    // Check if a ghost is eaten
+    pub fn isEaten(&self) -> bool {
+        self.ghost_state.eaten
+    }
+}
--- server/game_rs/src/pyo3_bindings/mod.rs
+++ server/game_rs/src/pyo3_bindings/mod.rs
@@ -2,8 +2,10 @@ use pyo3::prelude::*;
 use pyo3::types::PyModuleMethods;
 
 pub mod state_py;
+pub mod ghost_state_py;
 
 pub fn register_bindings(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<state_py::PyGameState>()?;
+    m.add_class::<ghost_state_py::PyGameGhostState>()?;
     Ok(())
 }
